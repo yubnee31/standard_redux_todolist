333befd89ba39161366f88f8cd4ee2d7e7c4aabb
yubnee31 <tydbqls@gmail.com>
2023-11-23 12:56:42 +0900

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import shortid from "shortid";
+// import { useSelector } from "react-redux";
+// import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { addTodo, deleteTodo, switchTodo } from "../redux/modules/todos";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div></div>;
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [todos, setTodos] = useState([]);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const id = shortid.generate();
+
+  // const todos = useSelector((state) => {
+  //   console.log(state);
+  //   return state;
+  // });
+
+  const addHandler = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: shortid.generate(),
+      title,
+      body,
+      isDone: false,
+    };
+    setTodos([...todos, newTodo]);
+    dispatch(addTodo(title, body));
+    setTitle("");
+    setBody("");
+  };
+
+  const deleteHandler = () => {
+    dispatch(deleteTodo(id));
+    // setTodos((preTodos) => preTodos.filter(todos.id !== preTodos.id));
+  };
+
+  const switchHandler = () => {
+    dispatch(switchTodo(id));
+  };
+
+  return (
+    <div>
+      <header>My Todo List</header>
+      <main>
+        <div>
+          <form onSubmit={addHandler}>
+            <input value={title} onChange={(e) => setTitle(e.target.value)} />
+            <input value={body} onChange={(e) => setBody(e.target.value)} />
+            <button type="submit">추가</button>
+          </form>
+        </div>
+        <h2>할일목록</h2>
+        {todos
+          .filter(function (todo) {
+            return todo.isDone === false;
+          })
+          .map(function (todo) {
+            return (
+              <div key={todo.id}>
+                <p>{todo.id}</p>
+                <p>{todo.title}</p>
+                <p>{todo.body}</p>
+                <p>완료: {todo.isDone.toString()}</p>
+                <button onClick={() => switchHandler(todo.id)}>
+                  {todo.isDone ? "UnDo" : "Done"}
+                </button>
+                <button onClick={() => deleteHandler(id)}>삭제</button>
+              </div>
+            );
+          })}
+        <h2>완료목록</h2>
+        {/* {todos.filter(function(todo){
+          return todo.isDone === true
+        }).map(function(todo){
+          return (
+            <div key={todo.id}>
+                <p>{todo.id}</p>
+                <p>{todo.title}</p>
+                <p>{todo.body}</p>
+                <p>완료: {todo.isDone.toString()}</p>
+          )
+        })
+        } */}
+      </main>
+    </div>
+  );
 };
 
 export default Home;
--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,26 +1,69 @@
 // import uuid from "react-uuid";
 import shortid from "shortid";
 
+const ADD_TODO = "todos/ADD_TODO";
+const DELETE_TODO = "todos/DELETE_TODO";
+const SWITCH_TODO = "todos/SWITCH_TODO";
+
+export const addTodo = (title, body) => ({
+  type: ADD_TODO,
+  payload: {
+    id: shortid.generate(),
+    title,
+    body,
+    isDone: false,
+  },
+});
+
+export const deleteTodo = (id) => ({
+  type: DELETE_TODO,
+  payload: {
+    id,
+  },
+});
+
+export const switchTodo = (id) => ({
+  type: SWITCH_TODO,
+  payload: {
+    id,
+  },
+});
+
 const initialState = [
   {
     id: shortid.generate(),
-    title: "",
-    body: "",
+    title: "제목1",
+    body: "내용1",
     isDone: false,
+  },
+  {
+    id: shortid.generate(),
+    title: "제목2",
+    body: "내용2",
+    isDone: true,
   },
 ];
 
 // 리듀서
 const todos = (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_TODO":
-      return; //TODO: 여기 작성
+    case ADD_TODO:
+      return [...state, action.payload];
 
-    case "DELETE_TODO":
-      return; //TODO: 여기 작성
+    case DELETE_TODO:
+      return state.filter((todo) => todo.id !== action.payload);
 
-    case "SWITCH_TODO":
-      return; //TODO: 여기 작성
+    case SWITCH_TODO:
+      return state.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return {
+            ...todo,
+            isDone: !todo.isDone,
+          };
+        } else {
+          return todo;
+        }
+      });
 
     default:
       return state;
